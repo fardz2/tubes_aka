09a9e426e4081d6a053c3971ad27e57621188c01
yanglekh <faridzmuhamad679@gmail.com>
2023-01-09 19:29:36 +0700

    aka

--- fibonacci-recursive.py
+++ fibonacci-recursive.py
@@ -1,9 +1,6 @@
 def recursive_fibonacci(n):
-    # If n = 0 or n = 1, return n
     if n == 0 or n == 1:
         return n
-    # Otherwise, return RECURSIVE-FIBONACCI(n-1) + RECURSIVE-FIBONACCI(n-2)
     else:
         return recursive_fibonacci(n-1) + recursive_fibonacci(n-2)
-
 print(recursive_fibonacci(2))
