3c2d727ee288cdc0aa341701d421c41c8b2e2346
yanglekh <faridzmuhamad679@gmail.com>
2023-01-10 23:45:03 +0700

    terbaru

--- permutasi_rekursif.py
+++ permutasi_rekursif.py
@@ -2,6 +2,7 @@ def permutasi_rekursif(n, r):
     if r == 0:
         return 1
     else:
-        return permutasi_rekursif(n-1, r) + permutasi_rekursif(n-1, r-1)
+         return (n * permutasi_rekursif(n - 1, r - 1))
+
 hasil = permutasi_rekursif(5, 2)
-print(hasil) 
+print(hasil)  
